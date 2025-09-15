0d475f64a1b8d6e7012372d0cced82ca71cdc147
cryskram <vageeshgn2005@gmail.com>
2025-09-16 01:21:32 +0530

    added horizontal padding

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const Home = () => {
   const [showProblems, setShowProblems] = useState(false);
 
   return (
-    <div className="flex flex-col items-center justify-start w-full min-h-screen bg-slate-200 text-black py-12">
+    <div className="flex flex-col items-center justify-start w-full min-h-screen bg-slate-200 text-black py-12 px-4">
       <div className="border-4 border-black rounded-lg max-w-2xl w-full mx-4 p-10 text-center bg-white shadow-[6px_6px_0px_black]">
         <h1
           className={`${strokerFont.className} text-5xl md:text-6xl font-bold tracking-wider`}
